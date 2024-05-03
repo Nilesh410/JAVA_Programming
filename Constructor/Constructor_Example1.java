27b808ea464aef146c01e36e1b86056a337b1e4d
Nilesh410 <shirudenileshs@gmail.com>
2024-05-03 23:55:18 +0530

    How to define constructor

---
+++ Constructor/Constructor_Example1.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    Demo()
+    {
+        System.err.println("From constructor");
+    }
+}
+public class Constructor_Example1 {
+    public static void main(String[] args) {
+        Demo d=new Demo();
+    }
+}
