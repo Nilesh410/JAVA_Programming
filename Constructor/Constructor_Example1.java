eeed97e37150b2dfdbec13e721f3c9a73d6cbd7c
Nilesh410 <shirudenileshs@gmail.com>
2024-05-05 00:00:03 +0530

    add new method

--- Constructor/Constructor_Example1.java
+++ Constructor/Constructor_Example1.java
@@ -4,9 +4,15 @@ class Demo
     {
         System.err.println("From constructor");
     }
+    Demo(int a)
+    {
+        System.out.println("a:",a);
+    }
 }
 public class Constructor_Example1 {
     public static void main(String[] args) {
         Demo d=new Demo();
+        Demo d1=new Demo(1);
+
     }
 }
