53d040e545102eb64471d19d54b2b61d494db5e7
Nilesh410 <shirudenileshs@gmail.com>
2024-08-22 10:26:12 +0530

    take an input from the user

--- First.java
+++ First.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class First {
     public static void main(String args[])
     {
         System.out.println("Hello");
+        System.out.println("Use Extension pack for java");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the variable value=");
+        int t;
+        t=sc.nextInt();
+        System.out.println("t="+t);
     }
 }
